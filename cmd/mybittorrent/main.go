930aaaf767913d5405eddf53278d84db6b220ba4
Swadhin2001 <2025.it18.swadhinpaul@gmail.com>
2024-10-08 16:43:37 +0530

    refactor: decodeBencode function to use bencode-go library for decoding bencoded strings and add support for decoding integers in bencoded strings

--- cmd/mybittorrent/main.go
+++ cmd/mybittorrent/main.go
@@ -15,9 +15,7 @@ var _ = json.Marshal
 func main() {
 
 	command := os.Args[1]
-
 	if command == "decode" {
-
 		bencodedValue := os.Args[2]
 		reader := strings.NewReader(bencodedValue)
 
@@ -40,6 +38,59 @@ func main() {
 		}
 
 		fmt.Println(string(jsonOutput))
+	} else if command == "info" {
+		data, error := os.ReadFile(os.Args[2])
+
+		if error != nil {
+			fmt.Println("Error fetchinf file:", error)
+			return
+		}
+
+		reader := strings.NewReader(string(data))
+
+		// Create a variable to hold the decoded data
+		// var decode interface{}
+
+		// Decode the bencoded data
+		decoded, err := bencode.Decode(reader)
+
+		if err != nil {
+			fmt.Println("Error decoding:", err)
+			return
+		}
+
+		// jsonOutput, err := json.Marshal(decoded)
+		// if err != nil {
+		// 	fmt.Println("Error converting to JSON:", err)
+		// 	return
+		// }
+		output := decoded
+		// Ensure the decoded data is a map
+		decodedMap, ok := output.(map[string]interface{})
+		if !ok {
+			fmt.Println("Error: Decoded data is not a map")
+			return
+		}
+
+		// Print the "announce" field
+		if announce, found := decodedMap["announce"]; found {
+			fmt.Printf("Tracker URL: %v\n", announce)
+		} else {
+			fmt.Println("Error: 'announce' field not found")
+		}
+
+		// Print the "info" field
+		if info, found := decodedMap["info"]; found {
+			infoMap, ok := info.(map[string]interface{})
+			if !ok {
+				fmt.Println("Error: 'info' field is not a map")
+				return
+			}
+			fmt.Printf("Length: %v\n", infoMap["length"])
+
+		} else {
+			fmt.Println("Error: 'info' field not found")
+		}
 	} else {
 		fmt.Println("Unknown command: " + command)
 		os.Exit(1)
